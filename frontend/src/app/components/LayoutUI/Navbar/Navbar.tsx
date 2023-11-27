310c10a03ef63940a0fd7fdbaab9821483e3f6f6
Shahul Hammeed <53307156+shahul01@users.noreply.github.com>
2023-11-27 10:33:01 +0530

    feat: added Navbar

---
+++ frontend/src/app/components/LayoutUI/Navbar/Navbar.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import styles from './navbar.module.css';
+
+
+type NavbarProps = {
+
+};
+
+
+const Navbar:NavbarProps = (props) => {
+  const {  } = props;
+
+  return (
+    <div className={styles['navbar']}>
+      <div className={styles['logo']}>
+        <h2><Link href='/'>Todo</Link></h2>
+      </div>
+      <div className={styles['links']}>
+        <Link href='/'>Home</Link>
+        <Link href='/todos'>Todos</Link>
+        <Link href='/auth'>SignIn</Link>
+      </div>
+
+    </div>
+  )
+};
+
+
+export default Navbar;

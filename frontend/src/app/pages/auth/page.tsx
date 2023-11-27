fa15c3428d38ce2e3daf1cbc573c43067eaa3c99
Shahul Hammeed <53307156+shahul01@users.noreply.github.com>
2023-11-27 10:34:57 +0530

    feat: added scaffold for /todos and /auth

---
+++ frontend/src/app/pages/auth/page.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+
+
+type AuthProps = {
+
+};
+
+
+const auth:AuthProps = (props) => {
+  const {  } = props;
+
+  return (
+    <div className='auth'>
+
+
+    </div>
+  )
+};
+
+
+export default auth;
---
+++ frontend/src/app/pages/todos/page.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+
+
+type TodosProps = {
+
+};
+
+
+const todos:TodosProps = (props) => {
+  const {  } = props;
+
+  return (
+    <div className='todos'>
+
+
+    </div>
+  )
+};
+
+
+export default todos;
